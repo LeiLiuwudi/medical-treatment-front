edf60ae58b07fdd76161ed57d0f87eff6c3822b7
LeiLiuwudi <2364485120@qq.com>
2021-11-23 16:50:48 +0800

    bugfix

--- src/api/api.js
+++ src/api/api.js
@@ -194,8 +194,8 @@ UrlMap.forEach((item) => {
       };
       return fetch(url, option).then((res) => res.json())
     }
-    if (item.url === "/record/download"){
-      return fetch(url,option).then((res) => res.json());
+    if (item.url === "/recognize/download"){
+      return fetch(url,option).then((res) => res);
     }
     // 通过fetch发送请求，第一个参数是请求地址。
     // json()返回一个被解析为JSON格式的promise对象
